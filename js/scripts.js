25253f218dad5944eff162cadde156ef21ba8c09
vbarlag <21960446+vbarlag@users.noreply.github.com>
2021-04-21 22:21:56 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Project InCIde - inCIde website v1.0.0 (https://incide.mus.auth.gr/)
+* Copyright 2013-2021 vbarlag
+* Licensed under MIT (https://github.com/vbarlag/incide-website/blob/main/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
